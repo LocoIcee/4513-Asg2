6fd673b0258580040cbad8428cb8084894b9b491
LocoIcee <62861210+LocoIcee@users.noreply.github.com>
2024-04-09 15:10:53 -0600

    img

--- F1/src/components/Circuit/circuitView.jsx
+++ F1/src/components/Circuit/circuitView.jsx
@@ -3,7 +3,6 @@ const CircuitView = (props) => {
     return(
         <>
             {props.viewCircuit ? (
-                <div class="flex justify-center h-screen items-center bg-gray-200 antialiased">
                     <div class="flex flex-col w-11/12 sm:w-5/6 lg:w-1/2 max-w-2xl mx-auto rounded-lg border border-gray-300 shadow-xl">
                         <div class="flex flex-row justify-between p-6 bg-white border-b border-gray-200 rounded-tl-lg rounded-tr-lg">
                             <h2>
@@ -18,11 +17,10 @@ const CircuitView = (props) => {
                             <button class="px-2 rounded-none border-black border-2 bg-slate-50 hover:bg-slate-500">
                                 Add Favorites
                             </button>
-                            <img src="assets/600x600.png"/>
+                            <img src="~/assets/600x600.png"/>
                             /* Map leaflet react compontent*/
                         </div>
                     </div>
-                </div>
             ) : (
                 <></>
             )}
