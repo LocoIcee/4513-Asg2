d4e5727a6b19439cea1d622b556f98cbccd20764
LocoIcee <62861210+LocoIcee@users.noreply.github.com>
2024-04-09 13:21:43 -0600

    err fix

--- F1/src/components/Circuit/circuitView.jsx
+++ F1/src/components/Circuit/circuitView.jsx
@@ -2,7 +2,7 @@ const CircuitView = (props) => {
 
     return(
         <>
-            {circuit != null & props.viewCircuit ? (
+            {props.circuit != null & props.viewCircuit ? (
                 <div class="flex justify-center h-screen items-center bg-gray-200 antialiased">
                     <div class="flex flex-col w-11/12 sm:w-5/6 lg:w-1/2 max-w-2xl mx-auto rounded-lg border border-gray-300 shadow-xl">
                         <div class="flex flex-row justify-between p-6 bg-white border-b border-gray-200 rounded-tl-lg rounded-tr-lg">
