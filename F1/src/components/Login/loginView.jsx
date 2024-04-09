e07188434ed921744cbfa28efa99852942b137a9
EggSaltedJuan <62862383+EggSaltedJuan@users.noreply.github.com>
2024-04-09 00:25:33 -0600

    Add files via upload

--- F1/src/components/Login/loginView.jsx
+++ F1/src/components/Login/loginView.jsx
@@ -1,21 +1,26 @@
 const LoginView = (props) => {
     return(
         <div>
-            <form>
-                <h2>
-                    Login
-                </h2>
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email"/>
-                <label htmlFor="pass">Password</label>
-                <input type="passord" id="pass"/>
-                <button onClick={() => props.doLogin()}>
-                    Login
-                </button>
-                <button onClick={() => props.doLogin()}>
-                    Register
-                </button>
-            </form>
+            <div class=" bg-[url('assets/jonathan-cosens-photography--at83Qidy7g-unsplash.jpg')] bg-no-repeat bg-cover flex flex-row min-h-screen justify-center items-center">
+                <div class=" bg-slate-100 border-4 border-spacing-x-52 border-black size-96">
+                    <form>
+                        <h2 class="flex justify-center font-mono font-bold text-2xl">
+                            Login
+                        </h2>
+                        <label htmlFor="email" class="block mb-2">Email</label>
+                        <input type="email" id="email"/>
+                        <label htmlFor="pass" class="block mb-2">Password</label>
+                        <input type="passord" id="pass"/>
+                        <button class="block rounded-none border-black border-2 bg-slate-50 hover:bg-slate-500" onClick={() => props.doLogin()}>
+                            Login
+                        </button>
+                        <button class="block rounded-none border-black border-2 bg-slate-50 hover:bg-slate-500" onClick={() => props.doLogin()}>
+                            Register
+                        </button>
+                    </form>
+                </div>
+            </div>
+            <div class="flex justify-center">unsplash.com</div>
         </div>
     )
 }
